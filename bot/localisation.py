ebfd676178c0bedbc5658d630258530d6d946e26
Naofumi <81846995+Nofumi@users.noreply.github.com>
2024-03-07 12:30:23 +0530

    Update localisation.py

--- bot/localisation.py
+++ bot/localisation.py
@@ -23,7 +23,7 @@ class Localisation:
     
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations."
     
-    COMPRESS_SUCCESS = "☆ @LostZoro17"
+    COMPRESS_SUCCESS = "@AdultJAV18 [ENCODED].mkv"
 
     COMPRESS_PROGRESS = "⏳ ETA: {}\n🚀 Progress: {}%"
 
